6719fc9cbd7b6e814fbc6bbf9b38ab3150af7bb3
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-24 23:52:25 +0530

    Fix: better service

--- services/chatRoomService.js
+++ services/chatRoomService.js
@@ -1,26 +1,36 @@
 import ChatRoomModel from "../models/chatRoom.js";
 import MessageModel from "../models/message.js";
+import UserModel from "../models/user.js";
 
 async function createRoom(creatorId, data) {
+
     const { roomName, participants } = data;
+    let finalParticipants = [...new Set([creatorId, ...participants])];
+
+    if (participants.length < 2) {
+        throw new Error("MIN_2_PARTICIPANTS_REQUIRED")
+    }
 
     const chatRoom = await ChatRoomModel.create({
         roomName,
         creatorId,
-        participants
+        participants: finalParticipants
     })
 
     return chatRoom;
 }
 
 async function sendMessage(senderId, roomId, message) {
+    const roomExist = await ChatRoomModel.findById(roomId);
 
-    if (!roomId) {
+    if (!roomExist) {
         throw new Error("ROOM_NOT_FOUND");
     }
 
-    if (!senderId) {
-        throw new Error("USER_NOT_FOUND");
+    if (!roomExist.participants.some(
+        (id) => id.toString() === senderId.toString()
+    )) {
+        throw new Error("NOT_A_PARTICIPANT");
     }
 
     const content = await MessageModel.create({
@@ -32,14 +42,22 @@ async function sendMessage(senderId, roomId, message) {
     return content;
 }
 
-async function getMessages(roomId) {
-    if (!roomId) {
+async function getMessages(senderId, roomId) {
+    const roomExist = await ChatRoomModel.findById(roomId);
+
+    if (!roomExist) {
         throw new Error("ROOM_NOT_FOUND");
+    }
+
+    if (!roomExist.participants.some(
+        (id) => id.toString() === senderId.toString()
+    )) {
+        throw new Error("NOT_A_PARTICIPANT");
     }
 
     const content = await MessageModel.find({
         roomId
-    })
+    }).sort({ createdAt: 1 })
 
     return content;
 }
