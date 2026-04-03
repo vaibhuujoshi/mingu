3378ffdb553f0adc230776a8db020951672e3268
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-04-03 19:39:50 +0530

    Fix: add fileUrlToPath for connecting log folder

--- utils/logger.js
+++ utils/logger.js
@@ -1,12 +1,22 @@
-import winston, { transports } from "winston";
+import winston from "winston";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const logger = winston.createLogger({
     level: "info",
     format: winston.format.json(),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({fileName: "logs/error.log", level: "error"}),
-        new winston.transports.File({fileName: "logs/combined.log"})
+        new winston.transports.File({
+            filename: path.join(__dirname, "../logs/error.log"),
+            level: "error"
+        }),
+        new winston.transports.File({
+            filename: path.join(__dirname,"../logs/combined.log")
+        })
     ]
 });
 
