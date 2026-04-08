58eb4f865c86baa98b734555edc4c63cf641482e
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-04-08 10:43:57 +0530

    Fix: import presence.socket.js

--- sockets/socketHandler.js
+++ sockets/socketHandler.js
@@ -1,6 +1,6 @@
 import messageConnection from "./message.socket.js";
 import typingConnection from "./typing.socket.js";
-import presenceConnection from "./presence.soclet.js";
+import presenceConnection from "./presence.socket.js";
 
 export default function registerSocketHandler(io, socket) {
     messageConnection(io, socket);
